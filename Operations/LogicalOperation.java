371fb7cd1215c68fe187bb75082f345820f51347
naseefu <naseefrahman90@gmail.com>
2025-01-09 11:59:03 +0530

    push

---
+++ Operations/LogicalOperation.java
@@ -0,0 +1,21 @@
+public class LogicalOperation {
+
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 6;
+        int c = 7;
+
+        if(a>b && a>c){
+            System.out.println("a is largest");
+        }
+        else if(b>a && b>c){
+            System.out.println("b is largest");
+        }
+        else{
+            System.out.println("c is largest");
+        }
+
+    }
+
+}
