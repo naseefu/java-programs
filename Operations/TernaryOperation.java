f43deef661d8ba7ef6af7f3d4a8e7cf30b5208f7
naseefu <naseefrahman90@gmail.com>
2025-01-09 11:56:28 +0530

    push TernaryOperation.java

---
+++ Operations/TernaryOperation.java
@@ -0,0 +1,13 @@
+public class TernaryOperation {
+
+    public static void main(String[] args) {
+
+        int a = 9;
+
+        String result = a%2==0?"Even":"Odd"; // ? : ->ternary operator
+
+        System.out.println(result);
+
+    }
+
+}
