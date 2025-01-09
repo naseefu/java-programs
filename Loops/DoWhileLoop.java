0368f5c7d1af2df3a64e0d15dee0065b20152a4d
naseefu <naseefrahman90@gmail.com>
2025-01-09 12:36:51 +0530

    push loops

---
+++ Loops/DoWhileLoop.java
@@ -0,0 +1,15 @@
+public class DoWhileLoop {
+
+    public static void main(String[] args) {
+
+        int i=3;
+
+        do{
+            System.out.println("Hai "+i);
+            i++;
+        }
+        while(i<=4);
+
+    }
+
+}
