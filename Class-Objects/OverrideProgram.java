81e9744ef8b5ac9844dc38cb08fb789f4465ea14
naseefu <naseefrahman90@gmail.com>
2025-01-10 19:52:34 +0530

    first

--- Class-Objects/OverrideProgram.java
+++ Class-Objects/OverrideProgram.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
+class Calculators{
+
+    public int add(int a, int b){
+        return a+b;
+    }
+
+    public int add(int a, int b, int c){
+        return a+b+c;
+    }
+
+    public double add(double a, int b){
+        return a+b; // double + int => double
+    }
+
+}
 
 public class OverrideProgram {
+
+    public static void main(String[] args) {
+
+        Calculators obj = new Calculators();
+        System.out.println(obj.add(1,2));
+        System.out.println(obj.add(1,2,3));
+        System.out.println(obj.add(5.4,2));
+
+    }
+
 }
